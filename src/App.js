a9e7fbaf1df1255094ec90cb6b813eb602836669
Hudhaifah <hudhaifahz@gmail.com>
2019-08-24 13:54:48 -0700

    cors-check

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import uuidv1 from 'uuid/v1'
+// import * from './services';
 
 function App() {
   return (
@@ -8,19 +10,35 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {localStorage.getItem('session') ? localStorage.getItem('session'): startList()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+function startList() {
+  var uuid = uuidv1()
+  localStorage.setItem('session', uuid)
+  fetch('https://commit-baby-names-backend.herokuapp.com/names')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+        return data + uuid;
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
 }
 
 export default App;
